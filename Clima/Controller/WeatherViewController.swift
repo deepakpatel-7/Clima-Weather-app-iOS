a971238ae8eec85c15ee1af1f134f0804da1747a
deepakpatel-7 <deepakpatel6957@gmail.com>
2024-02-08 13:23:02 +0530

    Updated the UI , the temperature and weather condition

--- Clima/Controller/WeatherViewController.swift
+++ Clima/Controller/WeatherViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class WeatherViewController: UIViewController ,UITextFieldDelegate {
+class WeatherViewController: UIViewController ,UITextFieldDelegate , WeatherManagerDelegate{
 
     @IBOutlet weak var conditionImageView: UIImageView!
     @IBOutlet weak var temperatureLabel: UILabel!
@@ -19,7 +19,9 @@ class WeatherViewController: UIViewController ,UITextFieldDelegate {
     var weatherManager = WeatherManager()
     override func viewDidLoad() {
         super.viewDidLoad()
+        weatherManager.delegate = self
         searchTextField.delegate = self
+        
         // Do any additional setup after loading the view.
     }
     
@@ -51,6 +53,18 @@ class WeatherViewController: UIViewController ,UITextFieldDelegate {
         }
          
         searchTextField.text = ""
+    }
+    
+    func didUpdateWeather(_ weatherManager : WeatherManager , weather:WeatherModel){
+        DispatchQueue.main.async {
+            self.temperatureLabel.text = weather.temperatureString
+            self.conditionImageView.image = UIImage(systemName: weather.ConditionName)
+        }
+        
+    }
+    
+    func didFailWithError(error: Error) {
+        print(error)
     }
     
 
--- Clima/WeatherManager.swift
+++ Clima/WeatherManager.swift
@@ -8,34 +8,42 @@
 
 import Foundation
 
+protocol WeatherManagerDelegate {
+    func didUpdateWeather(_ weatherManager : WeatherManager , weather:WeatherModel)
+    func didFailWithError(error: Error)
+}
+
 struct WeatherManager{
     let weatherURL =
     "https://api.openweathermap.org/data/2.5/weather?&appid=b8a515bdaee1d91c5d85d7c91b711841&units=metric"
     
+    var delegate:WeatherManagerDelegate?
     
     func fetchWeather(cityName:String){
         let urlString = "\(weatherURL)&q=\(cityName)"
-        performRequest(urlString: urlString)
+        performRequest(with: urlString)
     }
     
-    func performRequest(urlString: String){
+    func performRequest(with urlString: String){
         if let url = URL(string: urlString){
             let session = URLSession(configuration: .default)
             
             let task = session.dataTask(with: url) { (data, response, error ) in
                 if error != nil{
-                    print(error!)
+                    delegate?.didFailWithError(error: error!)
                     return
                 }
                 if let safeData = data{
-                    self.parseJSON(weatherData: safeData)
+                    if let weather = self.parseJSON(weatherData: safeData){
+                        self.delegate?.didUpdateWeather(self, weather: weather)
+                    }
                 }
             }
             
             task.resume()
         }
     }
-    func parseJSON(weatherData : Data){
+    func parseJSON(weatherData : Data) -> WeatherModel?{
         let decoder = JSONDecoder()
         do{
             let decodedData = try decoder.decode(WeatherData.self, from:weatherData)
@@ -44,11 +52,10 @@ struct WeatherManager{
             let name = decodedData.name
             
             let weather = WeatherModel(conditionId: id, cityName: name, temperature: temp)
-            print(weather.ConditionName)
-            print(weather.temperatureString)
-            
+            return weather
         } catch {
             print(error)
+            return nil
         }
     }
    
